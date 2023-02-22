47f3a78e0e8cbb09bd104f8d3d962dd3745b4e28
basicactor <careergo357@gmail.com>
2023-02-23 01:22:29 +0900

    ひな形作成完了

--- src/components/buttons/BaseButton.vue
+++ src/components/buttons/BaseButton.vue
@@ -1,15 +1,17 @@
 <template>
-  <v-app>
-    <v-main>
-      <router-view />
-    </v-main>
-  </v-app>
+  <v-btn :color="color" @click="$emit('click')"><slot /></v-btn>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "@vue/composition-api"
 
 export default defineComponent({
+  props: {
+    color: {
+      type: String,
+      default: "primary",
+    },
+  },
   setup() {
     return {}
   },
